67dac42915262dd9a9d1657ce5b2129e26ba3363
pig19980 <dotheg30@gmail.com>
2024-09-19 01:38:50 +0900

    feat: page fault가 나면 권한을 check하고 claim 하는 기능 추가

--- vm/vm.c
+++ vm/vm.c
@@ -232,13 +232,26 @@ static void vm_stack_growth(void *addr UNUSED) {}
 static bool vm_handle_wp(struct page *page UNUSED) {}
 
 /* Return true on success */
-bool vm_try_handle_fault(struct intr_frame *f UNUSED, void *addr UNUSED,
-						 bool user UNUSED, bool write UNUSED,
-						 bool not_present UNUSED) {
-	struct supplemental_page_table *spt UNUSED = &thread_current()->spt;
+bool vm_try_handle_fault(struct intr_frame *f, void *addr,
+						 bool user, bool write,
+						 bool not_present) {
+	struct supplemental_page_table *spt = &thread_current()->spt;
 	struct page *page = NULL;
 	/* TODO: Validate the fault */
 	/* TODO: Your code goes here */
+	if (!not_present) {
+		return false;
+	}
+	if (user && is_kernel_vaddr(pg_round_down(addr))) {
+		return false;
+	}
+	page = spt_find_page(spt, pg_round_down(addr));
+	if (page == NULL) {
+		return false;
+	}
+	if (write && !page->writable) {
+		return false;
+	}
 
 	return vm_do_claim_page(page);
 }
