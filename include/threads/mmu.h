19c2d0d80055044ca677af1906e71b595fd5e27b
pig19980 <dotheg30@gmail.com>
2024-09-25 16:52:08 +0900

    feat: pml4에 wriable과 관련된 함수 추가

--- include/threads/mmu.h
+++ include/threads/mmu.h
@@ -19,6 +19,8 @@ bool pml4_is_dirty(uint64_t *pml4, const void *upage);
 void pml4_set_dirty(uint64_t *pml4, const void *upage, bool dirty);
 bool pml4_is_accessed(uint64_t *pml4, const void *upage);
 void pml4_set_accessed(uint64_t *pml4, const void *upage, bool accessed);
+bool pml4_is_writable(uint64_t *pml4, const void *upage);
+void pml4_set_writable(uint64_t *pml4, const void *upage, bool writable);
 
 #define is_writable(pte) (*(pte)&PTE_W)
 #define is_user_pte(pte) (*(pte)&PTE_U)
